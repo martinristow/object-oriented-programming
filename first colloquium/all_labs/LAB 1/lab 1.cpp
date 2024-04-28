b2779e1357adc3cb83d30634953187f339fa128c
Martin Ristov <155776503+martinristow@users.noreply.github.com>
2024-04-28 17:01:35 +0200

    Add files via upload

---
+++ first colloquium/all_labs/LAB 1/lab 1.cpp
@@ -0,0 +1,31 @@
+# include <iostream>
+# include <cmath>
+#define PI 3.14
+////////////////////////////////102620////////////////////////////////////
+using namespace std;
+
+struct Cilindar {
+	int r,h;
+};
+float povrsina (Cilindar c){
+   
+    float p=(2*pow(c.r,2)*PI)+(2*c.r*PI*c.h);
+    return p;
+}
+
+int volumen (Cilindar c) {
+	int v=(pow(c.r,2)*PI*c.h);
+	return v;
+}
+
+int main (){
+    Cilindar c;
+    int v;
+    float p;
+    cout<<"Vnesi radius i vnesi gu visinata na cilindarot :  \n";
+    cin>>c.r>>c.h;
+    cout<<"Vnesenite vrednosti na cilindarot se : "<<c.r<<" , "<<c.h<<endl;
+    cout<<"Povrsinata na cilindarot iznesuva "<<povrsina(c)<<endl;
+    cout<<"Volumenot na cilindarot iznesuva "<<volumen(c)<<endl;
+    return 0;
+}
---
+++ first colloquium/all_labs/LAB 1/lab 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+////////////////////////////////102620////////////////////////////////////
+int main() {
+	char text[]="KING102620";
+	int brojac;
+	for(int i=0;text[i]!='\0';i++)
+	{
+	brojac++;
+	}
+cout<<"Zborot "<<text<<" ima "<<brojac<<" bukvi i brojki . \n";
+	return 0;
+}
